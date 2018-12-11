c48291c34c2c536ebf6f6068496ae212bebf20a3
James Barfield <barfieldjames@gmail.com>
2018-12-11 17:32:01 -0500

    S

--- os-config-files/createAll.sh
+++ os-config-files/createAll.sh
@@ -4,8 +4,8 @@ oc create -f chat-services.yaml
 oc create -f client-api.yaml
 oc create -f app-load.yaml
 oc create -f player-action-services.yaml
-oc create -f web-api.yaml
-oc create -f web-front-end.yaml
 oc create -f world-change-services.yaml
+oc create -f world-generator.yaml
+oc create -f world-portal.yaml
 
 oc create -f machine-agent.yaml
--- os-config-files/deleteAll.sh
+++ os-config-files/deleteAll.sh
@@ -4,8 +4,8 @@ oc delete -f chat-services.yaml
 oc delete -f client-api.yaml
 oc delete -f app-load.yaml
 oc delete -f player-action-services.yaml
-oc delete -f web-api.yaml
-oc delete -f web-front-end.yaml
 oc delete -f world-change-services.yaml
+oc delete -f world-generator.yaml
+oc delete -f world-portal.yaml
 
 oc delete -f machine-agent.yaml
