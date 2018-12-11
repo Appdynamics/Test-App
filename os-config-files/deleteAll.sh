4c1d9c737f2a9146bbddbcf639bef09da68dac52
James Barfield <barfieldjames@gmail.com>
2018-12-11 17:29:37 -0500

    S

--- os-config-files/deleteAll.sh
+++ os-config-files/deleteAll.sh
@@ -1,12 +1,11 @@
 oc delete -f action-response-services.yaml
 oc delete -f ai-services.yaml
-oc delete -f auth-services.yaml
 oc delete -f chat-services.yaml
 oc delete -f client-api.yaml
-oc delete -f demo-load.yaml
+oc delete -f app-load.yaml
 oc delete -f player-action-services.yaml
 oc delete -f web-api.yaml
 oc delete -f web-front-end.yaml
 oc delete -f world-change-services.yaml
 
-oc delete -f machine-agent.yaml
+# oc delete -f machine-agent.yaml
